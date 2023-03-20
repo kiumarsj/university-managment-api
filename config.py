50669832eb28a61e0f1ab33ed5a53c49e96bb412
kiumarsj <kiumars.javan@gmail.com>
2023-03-20 21:16:35 +0330

    switching to postgres

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = \
+        f'postgresql://{os.environ.get("POSTGRES_USER")}:{os.environ.get("POSTGRES_PASSWORD")}@localhost:5432/{os.environ.get("POSTGRES_DB")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email Configuration
